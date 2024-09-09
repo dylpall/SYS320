52035a28878dbc356ed9c5ad82f7c15a623f6066
Dylan <dylan.pallatroni@mymail.champlain.edu>
2024-09-09 12:11:58 -0400

    Week3 Lab

---
+++ week3/FunctionsandEventLogs.ps1
@@ -0,0 +1,28 @@
+﻿# get event login and logoff records
+#Get-EventLog -LogName System -Source Microsoft-Windows-WinLogon
+
+# Get login and logoff events and save as a variable
+# Get the last 14 days
+$loginouts = Get-EventLog -LogName System -Source Microsoft-Windows-WinLogon -After (Get-Date).AddDays(-14)
+
+$loginouts = @()
+for($i=0; $i -14; $loginouts.Count; $i++) 
+{
+
+    # Creating event property value
+    $event = ""
+    if($loginouts[$i].EventID -eq '21') {$event="Logon"}
+    if($loginouts[$i].EventID -eq '23') {$event="Logoff"}
+
+    # Creating user property value
+    $user = $loginouts[$i].ReplacementStrings[$i]
+
+    # Adding each new line (in form of a custom object) to our empty array
+    $loginoutsTable += [loginouts]@{"Time" = $loginouts[$i].ToString()
+                                      "Id" = $loginouts[$i].ToString()
+                                   "Event" = $event;
+                                    "User" = $user;
+                                    }
+} # End of for loop
+
+$loginoutsTable
