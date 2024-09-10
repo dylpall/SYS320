5901dc162dbcfe53738eb1fd27649dca4f492ad6
Dylan <dylan.pallatroni@mymail.champlain.edu>
2024-09-10 19:04:17 -0400

    week3 labs complete

---
+++ week3/DotNotation.ps1
@@ -0,0 +1,13 @@
+﻿. (Join-Path $PSScriptRoot StartAndShutDownTimes.ps1)
+. (Join-Path $PSScriptRoot FunctionsandEventLogs.ps1)
+
+clear
+
+# Get login and logoffs from the last 15 days
+$loginoutsTable = getTheLogs 15
+$loginoutsTable
+
+
+# get startup table and shutdown from past 25 days
+$startuptable = startupandshutdown 25
+$startuptable
--- week3/StartAndShutDownTimes.ps1
+++ week3/StartAndShutDownTimes.ps1
@@ -1,4 +1,29 @@
-function Get-StartupandShutDown {
-# Get startup and shutdown events from system
+clear
+function startupandshutdown($daysago){
+$startups = Get-EventLog -LogName System -Source EventLog -After (Get-Date).AddDays("-"+"$daysago") | Where-Object {$_.EventID -match "600[5-6]"}
 
-$events = Get-EventLog System -InstanceId 6005
+$startuptable = @()
+
+for($i=0; $i -lt $startups.Count; $i++) 
+{
+    # Creating event
+    $event = ""
+    if($startups[$i].InstanceID -eq 6005) {$event="Startup"}
+    if($startups[$i].InstanceID -eq 6006) {$event="Shutdown"}
+
+    # Creating user
+    $user = "system"
+
+    # Adding each new line (in form of a custom object) to our empty array
+    $startuptable += [pscustomobject]@{
+    "Time" = $startups[$i].TimeGenerated;
+    "Id" = $startups[$i].InstanceID;
+    "Event" = $event;
+    "User" = $user;
+}
+
+} # End of for loop
+
+return $startuptable
+}
+startupandshutdown(365)
