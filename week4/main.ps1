a9c1126c2492c48fb6cf4cee8b9b356894394992
Dylan <dylan.pallatroni@mymail.champlain.edu>
2024-09-17 17:48:37 -0400

    week4 apache lab

---
+++ week4/main.ps1
@@ -0,0 +1,8 @@
+﻿# Use dot notation to import the function from ApacheLogs.ps1
+. ./ApacheLogs.ps1
+
+
+$results = Retrieve-ApacheLogEntries -Page "index.html" -HttpCode "200" -Browser "Chrome"
+
+
+$results | Format-Table -AutoSize
