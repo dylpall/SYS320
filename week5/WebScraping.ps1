8730520e6a2a453e65d8abe84a8516b247b47e5b
Dylan <dylan.pallatroni@mymail.champlain.edu>
2024-09-26 19:04:23 -0400

    week 5 web scraping

--- week5/WebScraping.ps1
+++ week5/WebScraping.ps1
@@ -1,6 +1,7 @@
 ﻿clear
+
 $scraped_page = Invoke-WebRequest -TimeoutSec 10 http://10.0.17.8/ToBeScrapedHere.html
-<#
+
 # Get a count of the links in the page
 $scraped_page.Links.href
 
@@ -13,15 +14,69 @@ $scraped_page.Links | Select-Object outerText, href
 # Get outer text of every element with the tag h2
 $h2s=$scraped_page.ParsedHtml.body.getElementsByTagName("h2") | Select-Object outerText
 $h2s
-#>
+
 # Print innterText of every element that has the class as "div-1"
 $divs1=$scraped_page.ParsedHtml.body.getElementsByTagName("div") | where { `
 $_.getAttributeNode("class").Value -ilike "div-1"} | select innerText
 
 $divs1
 
-function gatherCLasses(){
-$page = Invoke-WebRequest -TimeoutSec 2 http://10.0.17.8/Courses-1.html
+function gatherClasses(){
+
+$page = Invoke-WebRequest -TimeoutSec 2 http://10.0.17.8/Courses.html
 
 # Get all the tr elements of HTML document
-$trs=$page.ParsedHtml.Links.getElementsByTagName("tr")
+$trs=$page.ParsedHtml.body.getElementsByTagName("tr")
+
+# Empty array to hold results
+$FullTable = @()
+for($i=1; $i -lt $trs.length; $i++){ # Going over every tr element
+
+    # Get every td element of current tr element
+    $tds = $trs[$i].getElementsByTagName("td")
+
+    # Want to separate start time and end time from one time field
+    $Times = $tds[5].innerText.split("-")
+
+    $FullTable += [pscustomobject]@{"Class Code" = $tds[0].innerText; `
+                                    "Title"      = $tds[1].innerText; `
+                                    "Days"       = $tds[4].innerText; `
+                                    "Time Start" = $Times[0]; `
+                                    "Time End"   = $Times[1]; `
+                                    "Instructor" = $tds[6].innerText; `
+                                    "Location"   = $tds[9].innerText; `
+                                }
+}
+return $FullTable
+}
+
+
+function daysTranslator($FullTable){
+
+# Go over every record in the table
+for($i=0; $i -lt $FullTable.length; $i++){
+
+# Empty array to hold days for every record
+$Days = @()
+
+# If  you see "M" --> Monday
+if($FullTable[$i].Days -ilike "M*"){ $Days += "Monday" }
+
+# If you see "T" followed by T,W,F -> Tuesday
+if($FullTable[$i].Days -ilike "*T[FTW]*") { $Days += "Tuesday" }
+
+# If you see "W" -> Wednesday
+if($FullTable[$i].Days -ilike "*W*") { $Days += "Wednesday" }
+
+# If you see "TH" -> Thursday
+if($FullTable[$i].Days -ilike "*TH*") { $Days += "Thursday" }
+
+# F -> Friday
+if($FullTable[$i].Days -ilike "*F") { $Days += "Friday" }
+
+# Make the switch
+$FullTable[$i].Days = $Days
+}
+return $FullTable
+}
+
---
+++ week5/main5.ps1
@@ -0,0 +1,35 @@
+﻿. (Join-Path $PSScriptRoot WebScraping.ps1)
+
+$FullTable = gatherClasses
+$NewTable = daysTranslator $FullTable
+
+# List all the classes of Instructor Furkan Paligu
+#$FullTable | Select-Object "Class Code", Title, Instructor, Location, Days, "Time Start", "Time End" | `
+#                where{ $_."Instructor" -ilike "Furkan*" }
+
+# List all the classes of Joyce 310 on mondays, only display class code and time
+# sort by start time
+#$FullTable | where-object{ ($_.Location -ilike "JOYC 310") -and ($_.days -contains "Monday") } | `
+#                Sort-Object -Property "Time Start" | `
+#                select "Time Start", "Time End", "Class Code"
+
+# Make a list of all the instructors that teach at least 1 course in
+# SYS, SEC, NET, FOR, CSI, DAT
+# Sort by name, and make it unique
+$ITSInstructors = $FullTable | Where-Object { `
+                                $_."Class Code" -ilike "SYS*" -or
+                                $_."Class Code" -ilike "NET*" -or
+                                $_."Class Code" -ilike "SEC*" -or
+                                $_."Class Code" -ilike "FOR*" -or
+                                $_."Class Code" -ilike "CSI*" -or
+                                $_."Class Code" -ilike "DAT*"
+                            } |
+                Sort-Object "Instructor" |
+                select "Instructor" -Unique 
+$ITSInstructors
+
+# Group all the instructors by the number of classes they are teaching
+$FullTable | Where-Object {$_.Instructor -in $ITSInstructors.Instructor } `
+           | Group-Object -Property "Instructor" `
+           | Select-Object Count,Name `
+           | Sort-Object Count -Descending
