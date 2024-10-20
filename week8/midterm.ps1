768462619be3e650792dd9182b77b810e032d5c2
Dylan <dylan.pallatroni@mymail.champlain.edu>
2024-10-20 18:25:25 -0400

    week 8 labs

--- week8/midterm.ps1
+++ week8/midterm.ps1
@@ -5,16 +5,54 @@ $url = Invoke-WebRequest -TimeoutSec 2 http://10.0.17.6/IOC.html
 $trs = $url.parsedhtml.body.GetElementsbytagname("tr")
 
 # empty array
-$Table = @()
+$iocTable = @()
 
 for($i-1; $i -lt $trs.length; $i++) {
     # get td element of tr elements
     $td = $trs[$i].getElementsbytagname("td")
-    $Table += [pscustomobject]@{
+    $iocTable += [pscustomobject]@{
                                 "Pattern" = $td[0].innertext;
                                 "Explanation" = $td[1].innertext;
                                 }
     }
-return $Table
+return $iocTable
 }
 # getIOC
+
+function GetApachelogs {
+    $logs = Get-Content C:\Users\champuser\SYS320\week8\access.log
+    $logTable = @()
+
+    for($i = 0; $i -lt $logs.Length; $i++) {
+        $SplitLogs = $logs[$i] -split " "
+        $logTable += [PSCustomObject]@{
+                                       "IP" = $SplitLogs[0];
+                                       "Time" = $SplitLogs[3].Trim('[');
+                                       "Method" = $SplitLogs[5].Trim('"');
+                                       "Page" = $SplitLogs[6];
+                                       "Protocol" = $SplitLogs[7];
+                                       "Response" = $SplitLogs[8];
+                                       "Referrer" = $SplitLogs[10];
+                                       }
+    }
+    return $logTable
+}
+# GetApachelogs | Format-Table
+
+function getIOCLogs(){
+    $logTable = GetApachelogs
+    $IOCTable = getIOC
+
+    $IOCLogTable = @()
+    for($i = 0; $i -lt $logTable.Count; $i++){
+        for($j = 0; $j -lt $IOCTable.Count; $j++){
+            if ($logTable[$i].Page -match $IOCTable[$j].Pattern){
+                $IOCLogTable += $logTable[$i]
+            }
+        }
+    }
+return $IOCLogTable
+
+}
+
+ getIOCLogs | Format-Table
