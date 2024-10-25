63c5f9146ea51fe5e225d84e540003e3e316c04e
dylpall <dylan.pallatroni@mymail.champlain.edu>
2024-10-25 11:56:41 -0400

    week 9 labs

--- week9/IPList.bash
+++ week9/IPList.bash
@@ -4,5 +4,18 @@
 # /24 only
 
 # Usage: bash IPList.bash 10.0.17
+[ $# -lt 1 ] && echo "Usage: $0  <Prefix>" && exit 1
 
-[  -d 1 ] -f echo "Usage:  <Prefix>"
+# Prefix is the first input taken
+Prefix=$1
+
+# Verify input length
+[ ${#Prefix} -lt 5 ] && \
+printf "Prefix length is too short\nPrefix example: 10.0.17\n" && \
+exit 1
+
+for i in {1..254}
+do
+	ping -c 1 $Prefix.$i | grep  "64 bytes from" | \
+	grep -o -E "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+done
