9e23cc3b742536203f42aa9478cc2508cc25102c
dylpall <dylan.pallatroni@mymail.champlain.edu>
2024-11-15 23:51:45 -0500

    week 12 labs

--- week12/readingtables.bash
+++ week12/readingtables.bash
@@ -1,13 +1,23 @@
 #! /bin/bash
+clear
 
+url="http://10.0.17.6/Assignment.html"
 
-curl -s "http://10.0.17.6/Assignment.html" -o page.html
+page=$(curl -sL "$url")
 
-xmllint --html --format page.html -o formatted_page.html 2>/dev/null
+temp=( $( \
+echo "$page" | \
+xmllint --xpath "//table[@id='temp']//td" - | \
+awk -F "[><]" '{print $3}'
+) )
 
-table1=$(awk '/<table/,/<\/table>/' formatted_page.html | head -n -1 | tail -n +2)
-table2=$(awk '/<table/,/<\/table>/ {if (++count == 2) {print}}' formatted_page.html)
+pres=( $( \
+echo "$page" | \
+xmllint --xpath "//table[@id='press']//td" - | \
+awk -F "[><]" '{print $3}'
 
-combined_table="<table>\n${table1}\n${table2}\n</table>"
+) )
 
-echo -e "$combined_table"
+for ((i=0; i<"${#temps[@]}"; i+=2)) {
+echo "${pres[$i]}  ${temp[$i]}  ${temps[$i+1]}"
+}
