9f3846d4bcca8af89beef251b515fdd9691f6ae6
dylpall <dylan.pallatroni@mymail.champlain.edu>
2024-12-04 16:11:08 -0500

    week15 complete

--- week15/networkchecker.bash
+++ week15/networkchecker.bash
@@ -4,8 +4,20 @@ myIP=$(bash myIP.bash)
 
 
 # Todo-1: Create a helpmenu function that prints help for the script
+function helpmenu() {
 
-
+	echo "		Help Menu		"
+	echo "----------------------------------"
+	echo "-n: Add -n for the script to use nmap"
+	echo " -n external: External NMAP scan"
+	echo " -n internal: Internal NMAP scan"
+	echo "-s: Add -s for the script to use ss"
+	echo " -s external: External SS scan"
+	echo " -s internal: Internal SS scan"
+	echo ""
+	echo "Usage: ${0} -n/-s external/internal"
+	echo "----------------------------------"
+}
 # Return ports that are serving to the network
 function ExternalNmap(){
   rex=$(nmap "${myIP}" | awk -F"[/[:space:]]+" '/open/ {print $1,$4}' )
@@ -18,8 +30,14 @@ function InternalNmap(){
 
 
 # Only IPv4 ports listening from network
-function ExternalListeningPorts(){
-
+function ExternalListeningPorts() {
+	elpo=$(ss -ltpn | \
+	awk -F"[[:space:]]+"  '{print $4, $6}' | \
+	grep -E "[0-9]{1-3}\." | \
+	grep -v "127" | \
+	awk -F"[[:space:]:(),]+" '{print $2,$4}' | \
+	tr -d "\""
+	)
 # Todo-2: Complete the ExternalListeningPorts that will print the port and application
 # that is listening on that port from network (using ss utility)
 
@@ -34,7 +52,10 @@ ilpo=$(ss -ltpn | awk  -F"[[:space:]:(),]+" '/127.0.0./ {print $5,$9}' | tr -d "
 
 
 # Todo-3: If the program is not taking exactly 2 arguments, print helpmenu
-
+if [ ! ${#} -eq 2 ]; then
+	helpmenu
+	exit
+fi
 # Todo-4: Use getopts to accept options -n and -s (both will have an argument)
 # If the argument is not internal or external, call helpmenu
 # If an option other then -n or -s is given, call helpmenu
@@ -42,4 +63,34 @@ ilpo=$(ss -ltpn | awk  -F"[[:space:]:(),]+" '/127.0.0./ {print $5,$9}' | tr -d "
 # For instance: -n internal => will call NMAP on localhost
 #               -s external => will call ss on network (non-local)
 
+while getopts "n:s:" option
+do
+	case $option in
+	n)
+		if [[ ${OPTARG} == "internal" ]]; then
+			InternalNmap
+			echo $rin
+		elif [[ ${OPTARG} == "external" ]]; then
+			ExternalNmap
+			echo $rex
+		else
+			helpmenu
+		fi
+	;;
+	s)
+		if [[ ${OPTARG} == "internal" ]]; then
+			InternalListeningPorts
+			echo $ilpo
+		elif [[ ${OPTARG} == "external" ]]; then
+			ExternalListeningPorts
+			echo $elpo
+		else
+			helpmenu
+		fi
 
+	;;
+	?)
+			echo "Invalid option: ${option}"
+			helpmenu
+	esac
+done
